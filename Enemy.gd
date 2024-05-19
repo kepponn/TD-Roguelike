16c67af6fdd8237f738eaaaffb84b5974505d995
perrorovic <perrorovic@gmail.com>
2024-05-19 12:41:51 +0700

    debugging: navigation settings and await process
    check 'Target.gd' and 'Enemy.gd' for more context

--- Enemy.gd
+++ Enemy.gd
@@ -3,22 +3,23 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 var accel = 10
+var direction = Vector3()
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	call_deferred("nav_setup")
 
-func _physics_process(delta):
-	var direction = Vector3()
-	
-	
+func nav_setup():
+	# await for 1 frame in process and nav error gone, wizard programming
+	await get_tree().physics_frame
 	nav.target_position = Global.final_target
 	direction = nav.get_next_path_position() - global_position
 	direction = direction.normalized()
-	
-	
+
+func _physics_process(delta):
+	nav_setup()
 	velocity = velocity.lerp(direction * SPEED, accel * delta)
-	
-	
 	move_and_slide()
--- Target.gd
+++ Target.gd
@@ -1,12 +1,23 @@
 extends Marker3D
 
+# Love letter from debugger <3
+# Fixed by changing the requirement of 'cell_size' in Project > Navigation > 3D
+# But did nothing new to existing navigation pathing what so ever, classic debugger
+# modules/navigation/nav_region.cpp:128 - Navigation map synchronization error. 
+# Attempted to update a navigation region with a navigation mesh that uses a `cell_size` of 0.10000000149012 
+# while assigned to a navigation map set to a `cell_size` of 0.25. 
+# The cell size for navigation maps can be changed by using the NavigationServer map_set_cell_size() function. 
+# The cell size for default navigation maps can also be changed in the ProjectSettings.
 
-# Called when the node enters the scene tree for the first time.
+# You could either bake the navigation from $"../NavigationRegion3D" or $"../NavigationRegion3D/Environtment"?
+
 func _ready():
+	# Would this be dynamic in the future? if so then the process flow is: (just-in-case)
+	# Check for changes and signal it back to here to do bake process and reroute the nav-process into new location target
+	# How? just call this_node._ready() and nav_node._ready()
 	$"../NavigationRegion3D".bake_navigation_mesh()
 	Global.final_target = position
 	print(Global.final_target)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	
+func _process(_delta):
 	Global.final_target = position

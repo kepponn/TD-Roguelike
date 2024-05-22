a64c228dc48452c52d373aea12f51854e5fc816b
perrorovic <perrorovic@gmail.com>
2024-05-22 23:15:19 +0700

    update: file folder sort and turret targeting fix
    please look into the bug in turret_projectile.gd

---
+++ scripts/Player.gd
@@ -0,0 +1,148 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var player_ableInteract: bool = false
+var player_isHoldingItem: bool = false
+var player_interactedItem
+var player_interactedItem_Temp
+
+func _physics_process(delta):
+	# Why does we have jump?
+	player_jump(delta)
+	
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+	player_rotation(direction)
+	player_movingItems()
+	exit()
+
+func exit():
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+
+func player_jump(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+func player_rotation(direction):
+	if direction != Vector3.ZERO:
+		var target_rotation = atan2(direction.x, direction.z) - PI / 2
+		# more of math wizardry, last param in lerp_angle() determine how fast the character rotate
+		$Node3D.rotation.y = lerp_angle($Node3D.rotation.y, target_rotation, 0.25)
+	# rotation of items are being modified by this code while being held on hand
+	# and repaired back to grid by check_grid()
+
+func check_grid(import_pos, export_pos):
+	# read the docs more, round() function is just what we want, it round .5 to up
+	# could be done directly in-line because this support vector3
+	# export_pos.position.x = round(import_pos.global_position.x) # X-AXIS
+	var x = round(import_pos.global_position.x)
+	# The Y-AXIS is depend on how the %"Interaction Zone" placement is in the world. before it was 1.501, now it is 1.33
+	# export_pos.position.y = round(import_pos.global_position.y) # Y-AXIS (sometimes messed up by the character interact area)
+	var y = round(import_pos.global_position.y)
+	# export_pos.position.z = round(import_pos.global_position.z) # Z-AXIS
+	var z = round(import_pos.global_position.z)
+	# disable janky item rotation when held by player to align with grid
+	export_pos.rotation.y = 0 # this is being applied instantly
+	# Return the value in vector3 for future processing by tweens
+	return Vector3(x, y, z)
+
+func player_holdItem(item):
+	item.reparent(%"Holded Item", true) # Change the item parent into `%"Holded Item"` which reside in player node
+	item.set_collision_layer_value(1, false) # Remove the collision layer from the item while being held in-hand
+	#item.position = Vector3(0.5, 1, 0) # Set item position to be on top of player
+	var holdItem_Tween = get_tree().create_tween()
+	holdItem_Tween.tween_property(item, "position", Vector3(0.5, 1, 0), 0.15)
+
+func player_putItem(item):
+	item.reparent(%Item, true) # Change the item parent into `%Item` node
+	#item.set_collision_layer_value(1, true) # Enable the collision layer of the item
+	# Check the grid of item when putting down to the world
+	var putItem_Tween = get_tree().create_tween()
+	putItem_Tween.tween_property(item, "position", check_grid(%"Interaction Zone", item), 0.15)
+	await get_tree().create_timer(0.15).timeout
+	item.set_collision_layer_value(1, true)
+
+func player_swapItem(held_item, ground_item):
+	held_item.reparent(%Item, true) 
+	held_item.set_collision_layer_value(1, true)
+	held_item.position = ground_item.position # Swap the position property from held item to ground item
+	held_item.rotation.y = 0 # Repair the Y-AXIS rotation to default
+
+func player_checkItemRange(item, enable: bool = true):
+	# need to add some regex to check for all the name id of turret
+	# for example all turret name start with 'turret_name_affix_suffix_whatever'
+	# check for match regex on 'turret' and be happy
+	if item.name == "Turret" and enable:
+		# modify temp variable to match the item range and show the area
+		$"Node3D/Holded Item/Item Range".mesh.top_radius = player_interactedItem.attack_range
+		$"Node3D/Holded Item/Item Range".mesh.bottom_radius = player_interactedItem.attack_range
+		$"Node3D/Holded Item/Item Range".visible = true
+	if item.name == "Turret" and !enable:
+		$"Node3D/Holded Item/Item Range".visible = false
+
+func player_movingItems():
+	# PICK UP ITEM - NOT HOLDING item and HAVE INTERACTABLE item
+	if player_ableInteract == true and player_isHoldingItem == false and Input.is_action_just_pressed("interact"):
+		player_isHoldingItem = true
+		# When the player decided to interact with the current item `player_interactedItem_Temp` (which always changing depend on the circumstance)
+		# It will save the `player_interactedItem_Temp` into `player_interactedItem` to be used
+		player_interactedItem = player_interactedItem_Temp
+		player_holdItem(player_interactedItem)
+		player_checkItemRange(player_interactedItem)
+		print("Pick-up " + str(player_interactedItem) + " from " + str(player_interactedItem.position))
+		# Maybe not needed because if item is moved over character's head
+		# Then interaction zone body exited will triggered to change player_ableInteract to false
+		player_ableInteract = false
+		
+	# DROP DOWN ITEM - HOLDING an item and NOT INTERACTING with other items
+	elif player_ableInteract == false and player_isHoldingItem == true and  Input.is_action_just_pressed("interact"):
+		# Should change the name of item placeholder into someting more unique, for now it stand as %Item
+		player_putItem(player_interactedItem)
+		player_checkItemRange(player_interactedItem, false)
+		
+		print("Dropping " + str(player_interactedItem) + " to " + str(player_interactedItem.position))
+		# print(%"Interaction Zone".global_position)
+		# print(player_interactedItem.position)
+		player_isHoldingItem = false
+		
+	# SWAP ITEM - HOLDING an items and HAVE INTERACTABLE item
+	elif player_ableInteract == true and player_isHoldingItem == true and Input.is_action_just_pressed("interact"):
+		print("Swapping " + str(player_interactedItem) + " with " + str(player_interactedItem_Temp))
+		# All this line is the basic for item swapping
+		# Swap held item property to on-ground item property
+		player_swapItem(player_interactedItem, player_interactedItem_Temp)
+		player_checkItemRange(player_interactedItem, false)
+		# Take on-ground item
+		player_interactedItem = player_interactedItem_Temp
+		player_holdItem(player_interactedItem)
+		player_checkItemRange(player_interactedItem, true)
+
+func _on_interaction_zone_body_entered(body):
+	if body.get_parent().name == 'Item':
+		player_interactedItem_Temp = body
+		player_ableInteract = true
+		#print("player ABLE to interact with " + body.name)
+
+func _on_interaction_zone_body_exited(body):
+	if body.get_parent().name == 'Item':
+		player_ableInteract = false
+		#print("player UNABLE to interact with " + body.name)
---
+++ scripts/Turret.gd
@@ -0,0 +1,126 @@
+extends StaticBody3D
+
+var projectile: PackedScene = preload("res://scene/turret_projectile.tscn")
+
+var enemies_inRange = []
+var enemy_locked
+var able_toShoot: bool = false
+
+@export var attack_range:float
+
+@onready var range_radius: CollisionShape3D = $Range/CollisionShape3D
+@onready var raycast: RayCast3D = $RayCast3D
+
+func _ready():
+	range_radius.shape.radius = attack_range
+	raycast.target_position.z = -attack_range
+	$RayCast3D.hide()
+
+func _process(_delta):
+	lock_on_is_easy()
+	#lockon()
+	shoot()
+
+# BOTH FUNCTION WORKING FINE, USE WHATEVER YOU WANT
+# THIS IS THE MAGIC SPELL
+# REMEMBER THIS WELL AND RECITE IT AFTER ME
+# $RayCast3D.force_raycast_update()
+
+# BUT... it still wont target the enemy which the closest to the target because there is currently no param to check for it
+
+func lock_on_is_easy():
+	if !enemies_inRange.is_empty(): # check array empty state
+		# the target will always be the array[0]
+		$RayCast3D.look_at(enemies_inRange[0].position) # raycast to the target
+		$RayCast3D.force_raycast_update() # faster reporting for raycast
+		if $RayCast3D.is_colliding(): # check does the raycast even colide with something?
+			# if the collider is not child of 'Enemies' and the array have more than 1 data
+			# put the array[0] data into back of the array itself and delete it (same as push_back())
+			# this will force the array to re-index itself and now we have different data for array[0]
+			if $RayCast3D.get_collider().get_parent().name != 'Enemies' and enemies_inRange.size() > 1:
+				print("Vision obstructed to " + str(enemies_inRange[0].name) + " pushing it to back of array")
+				able_toShoot = false
+				$RayCast3D.debug_shape_custom_color = Color(255,0,0)
+				enemies_inRange.append(enemies_inRange[0])
+				enemies_inRange.remove_at(0)
+				print("New array for targeting: " + str(enemies_inRange))
+			# check if the raycast collide with 'Enemies', then execute turret movement and shoot, etc
+			elif $RayCast3D.get_collider().get_parent().name == 'Enemies':
+				able_toShoot = true
+				$RayCast3D.show()
+				$Head.look_at(enemies_inRange[0].position)
+				#print("Shooting at " + str(enemies_inRange[0].name))
+				#print("Detecting " + str(enemies_inRange.size()) + " enemies")
+				$RayCast3D.debug_shape_custom_color = Color(0,255,0)
+			# important fail-save for this statement, leave it with atleast something to do
+			else:
+				able_toShoot = false
+				$RayCast3D.hide()
+				#print("No enemies detected")
+				$RayCast3D.debug_shape_custom_color = Color(255,0,0)
+
+func shoot():
+	var direction: Vector3
+	#i dont know why if $"Attack Speed".time_left <= 0.3 set to <= 0.0 all code below didn't work except for $"Attack Speed".start(1)
+	if (enemy_locked != null or !enemies_inRange.is_empty()) and $"Attack Speed".time_left <= 0.0 and able_toShoot == true:
+		if enemy_locked != null:
+			direction = (enemy_locked.position - position).normalized()
+		elif !enemies_inRange.is_empty():
+			direction = (enemies_inRange[0].position - position).normalized()
+		var turret_projectile = projectile.instantiate()
+		# if you want to shoot while still holding it maybe make projectile as unique or use absolute path to it
+		$"../../../Projectile".add_child(turret_projectile,true)
+
+		#basically copy all of $"Head/Spawn Point" global transform(rotation, scale, position), to projectile
+		turret_projectile.transform = $"Head/Spawn Point".global_transform
+		
+		#direction used to set projectile movement direction
+		turret_projectile.set_direction = direction
+		
+		#restart timer so it can shoot again
+		$"Attack Speed".start(1)
+	
+
+func lockon():
+	#NOTES : Raycast are not infinitly long. it's length need to be change on Raycast3D -> Inspector -> Target Position
+	#and it can be changed by code by using $RayCast3D.target_position
+	#if there is enemy inside attack RANGE, turret will lock into the first enemy
+	
+	#need better targeting system, 
+	
+	if enemies_inRange.size() != 0:
+		enemy_locked = enemies_inRange[0]
+		#lock raycast look at to the first enemy in the enemies_inRange array
+		$RayCast3D.look_at(enemy_locked.position)
+		$RayCast3D.force_raycast_update()
+		
+		#check if raycast colliding with any 3D bodies
+		if $RayCast3D.is_colliding() and $RayCast3D.get_collider()!= null:
+			#can save any data of the collided body, in this case we only want the parent name
+			#var collided = $RayCast3D.get_collider().get_parent().name
+			#if collided's parent name is Enemies then it mean raycast able to hit enemies successfully and
+			#not blocked by any 3D body, and then make turret's $Body to look at the locked enemy
+			if $RayCast3D.get_collider().get_parent().name == 'Enemies':
+				able_toShoot = true
+				$Head.look_at(enemy_locked.position)
+				$RayCast3D.debug_shape_custom_color = Color(0,255,0)
+			else:
+				able_toShoot = false
+				enemies_inRange.append(enemies_inRange[0])
+				enemies_inRange.remove_at(0)
+				$RayCast3D.debug_shape_custom_color = Color(174,0,0)
+	else:
+		able_toShoot = false
+		$RayCast3D.debug_shape_custom_color = Color(174,0,0)
+
+func _on_range_body_entered(body):
+	if body.get_parent().name == 'Enemies':
+		print("Append " + str(body))
+		enemies_inRange.append(body)
+		print("Current array " + str(enemies_inRange))
+
+func _on_range_body_exited(body):
+	if body.get_parent().name == 'Enemies':
+		print("Erase " + str(body))
+		enemies_inRange.erase(body)
+		print("Current array " + str(enemies_inRange))

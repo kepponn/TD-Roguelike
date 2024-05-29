ee408f37530bcadc79176342ae48e8f2c6f8f21a
perrorovic <perrorovic@gmail.com>
2024-05-29 16:23:49 +0700

    hotfix: shop terminal collision

--- scene/shop_terminal.gd
+++ scene/shop_terminal.gd
@@ -17,12 +17,12 @@ func _process(_delta):
 			#$CollisionShape3D.disabled = false
 
 func _on_item_placeholder_body_entered(body):
-	if body.name != "Shop" and !body.is_class("GridMap"):
+	if body.name != "Shop" and !body.is_class("GridMap") and body.name != "Player":
 		print(body.name, " Entered")
 		set_collision_layer_value(1,false)
 
 func _on_item_placeholder_body_exited(body):
-	if body.name != "Shop" and !body.is_class("GridMap"):
+	if body.name != "Shop" and !body.is_class("GridMap") and body.name != "Player":
 		set_collision_layer_value(1,true)
 		print(body.name, " Exited")
 
--- scripts/wall_spiked.gd
+++ scripts/wall_spiked.gd
@@ -3,7 +3,7 @@ extends Wall_Parent
 func _on_area_3d_body_entered(body):
 	if body.get_parent().name == 'Enemies' and $AttackSpeed.time_left <= 0.0:
 		body.hit(1)
-		$AttackSpeed.start(5)
+		$AttackSpeed.start(3)
 		$SpikeSfx.play()
 		var spike_Tween = get_tree().create_tween()
 		spike_Tween.tween_property($Models/wall_spike_only, "scale", Vector3(1.3, 1, 1.3), 0.25)
